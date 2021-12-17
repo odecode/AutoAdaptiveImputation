1646f313be71a503784d208dadeb703e41edbbf1
Otto <westerlundotto@gmail.com>
2021-12-17 23:55:33 +0200

    something wrong with maskstore or set1_epi32

--- related_avx2.h
+++ related_avx2.h
@@ -18,7 +18,7 @@ using namespace std::chrono;
 using namespace std;
 
 struct relatedUsers{
-    int *rel_users;
+    int32_t *rel_users;
     int rel_users_size;
     int user;
 };
@@ -32,19 +32,29 @@ relatedUsers get_related_users(int user, int item, int** matrix, int nusers, int
     Returns 
 
     */
-    int *rel_users_arr = (int*) malloc(nusers*sizeof(int));
-    int veclen = 4;
+    int32_t *rel_users_arr = (int32_t*) malloc(nusers*sizeof(int32_t));
+    int veclen = 8;
     int leftOverFromArrayEnd = nusers % veclen;
-    __m256i minus1 = _mm256_set1_epi32(-1);
-    __m256i maskones = _mm256_set1_epi32(1);
+    const int32_t negone = -1;
+    const int32_t one = 1; 
+    __m256i minus1 = _mm256_set_epi32(negone,negone,negone,negone,negone,negone,negone,negone);
+    __m256i maskones = _mm256_set_epi32(one,one,one,one,one,one,one,one);
+
+    
+   for (int i = 0; i < veclen; i++)
+   {
+       //zeromask[i] = 0;
+     // cout << minus1[i] << " ";
+   }
+   cout << endl;
 
     for (int i = 0; i < nusers-leftOverFromArrayEnd; i+=veclen)
     {
-        // cout << rel_users_arr[i] << " " << rel_users_arr[i+1] << " " << rel_users_arr[i+2] << " " << rel_users_arr[i+3] << " ";
+        // cout << rel_users_arr[i] << " " << rel_users_arr[i+1] << " " << rel_users_arr[i+2] << " " << rel_users_arr[i+3] << " " << rel_users_arr[i+4] << " " << rel_users_arr[i+5] << " " << rel_users_arr[i+6] << " " << rel_users_arr[i+7] << " ";
         // cout << endl;
         _mm256_maskstore_epi32(&rel_users_arr[i],minus1,minus1);
         //_mm256_store_si256(rel_users_arr[i],minus1);
-        // cout << rel_users_arr[i] << " " << rel_users_arr[i+1] << " " << rel_users_arr[i+2] << " " << rel_users_arr[i+3] << " ";
+        // cout << rel_users_arr[i] << " " << rel_users_arr[i+1] << " " << rel_users_arr[i+2] << " " << rel_users_arr[i+3] << " " << rel_users_arr[i+4] << " " << rel_users_arr[i+5] << " " << rel_users_arr[i+6] << " " << rel_users_arr[i+7] << " ";
         // cout << endl << endl;
         //rel_users_arr[i] = -1; //the original code that we vectorize
     }
@@ -52,7 +62,7 @@ relatedUsers get_related_users(int user, int item, int** matrix, int nusers, int
     // store -1 into the last part of array, which didnt fit into vectors
     for (int i = nusers-leftOverFromArrayEnd; i < nusers; i++)
     {
-        rel_users_arr[i] = -1;
+        rel_users_arr[i] = (int32_t) -1;
     }
     
 
@@ -67,14 +77,15 @@ relatedUsers get_related_users(int user, int item, int** matrix, int nusers, int
         UPDATE: Apparently it compiles to 64-bit ints (at least when I debug the cmpeq instruction) so we change veclen to 4
         since then we can fit 256/64 = 4 
     */
-
-   __m256i mask = _mm256_set1_epi32(0);
-//   __m256i mask;
+   const int32_t zero = 0;
+   __m256i zeromask = _mm256_set1_epi64x(zero);
+// //   __m256i mask;
 //    for (int i = 0; i < veclen; i++)
 //    {
-//        mask[i] = 0;
-//       // cout << mask[i] << endl;
+//        //zeromask[i] = 0;
+//       cout << zeromask[i] << " ";
 //    }
+   cout << endl;
    
     for (int user2 = 0; user2 < nusers - leftOverFromArrayEnd; user2 += veclen){
         __m256i ratingdata;
@@ -82,37 +93,40 @@ relatedUsers get_related_users(int user, int item, int** matrix, int nusers, int
         
         // load a vector of ratings from matrix (from user2 to user2 + 8)
         // create vector of users
+        cout << "rating data " << endl;
         for (int i = 0; i < veclen; i++)
         {
-            ratingdata[i] = matrix[user2+i][item];
-            uservec[i] = user2+i;
-            //cout << data[i] << " ";
+            ratingdata[i] = (int32_t) matrix[user2+i][item];
+            uservec[i] = (int32_t) user2+i;
+            cout << ratingdata[i] << " ";
         }
-        //cout << endl;
+        cout << endl;
         
         
         /* Compare the ratings with mask of 0's to get a boolean mask telling if users are related or not */ 
-        __m256i related = _mm256_cmpeq_epi64(ratingdata,mask);
+        __m256i related = _mm256_cmpeq_epi32(ratingdata,zeromask);
+        cout << "related " << endl;
+       for (int i = 0; i < 8; i++)
+       {
+           cout << related[i] << " ";
+       }
+       cout << endl;
         
-        //related = _mm256_mul_epi32(related,minus1);
-        for (int i = 0; i < veclen; i++)
+    cout  << "before maskstore" << endl;
+        for (int i = 0; i < 8; i++)
     {
-        cout << related[i] << " ";
+        cout << rel_users_arr[user2+i] << " ";
+
     }
     cout << endl;
-    //     for (int i = 0; i < veclen; i++)
-    // {
-    //     cout << rel_users_arr[user2+1] << " ";
-    // }
-    // cout << endl;
         _mm256_maskstore_epi32(&rel_users_arr[user2],related,uservec);
-    // for (int i = 0; i < veclen; i++)
-    // {
-    //     cout << rel_users_arr[user2+1] << " ";
-    // }
-    // cout << endl;
-    // cout << endl;
-    // cout << endl;
+    cout  << "after maskstore" << endl;
+        for (int i = 0; i < 8; i++)
+    {
+        cout << rel_users_arr[user2+i] << " ";
+
+    }
+    cout << endl;
 
     }
     for (int user2 = nusers-leftOverFromArrayEnd; user2 < nusers; user2++)
@@ -122,7 +136,7 @@ relatedUsers get_related_users(int user, int item, int** matrix, int nusers, int
         //cout << "in get rel users 3\n";
             related = matrix[user2][item] == 0 ? false : true;
             if(related){
-                rel_users_arr[user2]=user2;
+                rel_users_arr[user2]=(int32_t) user2;
             }
         }
     }
